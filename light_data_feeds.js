96b7449ceb6a1700c5ca5a8c23a861aac7956bb3
tony <tonyofbyteball@users.noreply.github.com>
2022-04-01 21:21:08 +0300

    watch data feeds and update values in real time

--- light_data_feeds.js
+++ light_data_feeds.js
@@ -1,5 +1,8 @@
 "use strict";
 const data_feeds = require('ocore/data_feeds.js');
+const network = require('ocore/network.js');
+const eventBus = require('ocore/event_bus.js');
+const string_utils = require('ocore/string_utils.js');
 const dag = require('./dag.js');
 
 const ORACLE_DATA_LIFETIME = 60 * 1000;
@@ -27,6 +30,8 @@ async function updateDataFeed(oracle_address, feed_name, bForce) {
 	let oracle = getOracle(oracle_address, feed_name);
 	if (oracle.ts > Date.now() - ORACLE_DATA_LIFETIME && !bForce)
 		return console.log(`oracle ${oracle_address}:${feed_name} data is up to date`);
+	if (oracle.ts === 0) // new oracle
+		network.addTempLightWatchedAddress(oracle_address);
 	console.log(`will update oracle ${oracle_address}:${feed_name}`);
 	const prev_value = oracle.value;
 	try {
@@ -42,5 +47,21 @@ async function updateDataFeed(oracle_address, feed_name, bForce) {
 	console.log(`oracle ${oracle_address}:${feed_name}`, bUpdated ? `updated from ${prev_value} to ${oracle.value}` : `same value ${prev_value}`);
 	return bUpdated;
 }
+
+eventBus.on('new_joint', objJoint => {
+	const objUnit = objJoint.unit;
+	const author_addresses = objUnit.authors.map(a => a.address);
+	const df_message = objUnit.messages.find(m => m.app === 'data_feed');
+	if (!df_message)
+		return console.log('no data feed in unit', objUnit.unit);
+	const df = df_message.payload;
+	for (let address of author_addresses)
+		for (let oracle of oracles)
+			if (oracle.address === address && df[oracle.feed_name]) {
+				console.log(`received new value of data feed ${oracle.address}:${oracle.feed_name}`, df[oracle.feed_name]);
+				oracle.value = string_utils.getFeedValue(df[oracle.feed_name]);
+				oracle.ts = Date.now();
+			}
+});
 
 exports.updateDataFeed = updateDataFeed;
